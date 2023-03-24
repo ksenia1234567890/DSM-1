8dd04b2cf4f21c1a90f9eb7c71a9ff78ffa941fd
ksenia 1234567890 <eurosetv3.04.05.14@gmail.com>
2023-03-24 13:24:03 +0300

    Добавьте файлы проекта.

---
+++ lr/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace lr
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label9 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label9.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label9.Location = new System.Drawing.Point(168, 247);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(394, 17);
+            this.label9.TabIndex = 26;
+            this.label9.Text = "После ввода чисел дважды щёлкните по текстовому полю";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label7.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label7.Location = new System.Drawing.Point(227, 221);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(256, 17);
+            this.label7.TabIndex = 25;
+            this.label7.Text = "Пример, 78*96*5*58*96*44*1*25*145*7";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(99, 579);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(125, 28);
+            this.textBox4.TabIndex = 24;
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.button7.Location = new System.Drawing.Point(509, 556);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(171, 51);
+            this.button7.TabIndex = 23;
+            this.button7.Text = "Произвести сортировку";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label6.Location = new System.Drawing.Point(252, 391);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(206, 20);
+            this.label6.TabIndex = 22;
+            this.label6.Text = "Отсортированный массив";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.textBox3.Location = new System.Drawing.Point(191, 427);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(327, 92);
+            this.textBox3.TabIndex = 21;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label5.Location = new System.Drawing.Point(207, 44);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(292, 20);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "Выберете способ получения массива";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label4.Location = new System.Drawing.Point(207, 185);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(309, 20);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "Или введите 10 чисел через звёздочку\r\n";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.textBox2.Location = new System.Drawing.Point(191, 279);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(327, 80);
+            this.textBox2.TabIndex = 18;
+            this.textBox2.DoubleClick += new System.EventHandler(this.textBox2_DoubleClick);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.button5.Location = new System.Drawing.Point(393, 95);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(125, 48);
+            this.button5.TabIndex = 17;
+            this.button5.Text = "Рандом";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.button4.Location = new System.Drawing.Point(191, 95);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(125, 48);
+            this.button4.TabIndex = 16;
+            this.button4.Text = "Загрузка из файла";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label8.Location = new System.Drawing.Point(95, 556);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(58, 20);
+            this.label8.TabIndex = 27;
+            this.label8.Text = "Время";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(753, 648);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Name = "Form1";
+            this.Text = "Сортировка";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ lr/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lr
+{
+    public partial class Form1 : Form
+    {
+        public int[] array = new int[10];
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string way = "D:\\\\3 курс\\6 семестр\\рпм\\массив.txt";
+            string line = File.ReadAllText(way);
+            int y = 0;
+            foreach (var i in line.Split('*'))
+            {
+                array[y] = Convert.ToInt32(i);
+                y++;
+            }
+            string outcome = "";
+            for (int i = 0; i < array.Length; i++)
+            {
+                outcome += Convert.ToString(array[i]) + " ";
+            }
+            MessageBox.Show($"Массив из файла\n{outcome}");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string line = "";
+            Random numeric = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = numeric.Next(1, 100);
+                line += Convert.ToString(array[i]) + " ";
+            }
+
+            MessageBox.Show($"Массив из случайных чисел:\n{line}");
+        }
+
+        private void textBox2_DoubleClick(object sender, EventArgs e)
+        {
+            try
+            {
+                string line = textBox2.Text;
+                int j = 0;
+                foreach (var i in line.Split('*'))
+                {
+                    array[j] = Convert.ToInt32(i);
+                    j++;
+                }
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Неверный формат. Попробуйте ввести заново");
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            string array_line = "";
+            Stopwatch time = new Stopwatch();
+            time.Start();
+            int count = 0;
+            for (int i = 1; ; i++)
+            {
+                if (i == array.Length)
+                {
+                    i = 1;
+                }
+                int y = i - 1;
+                if (array[y] < array[i])
+                {
+                    count++;
+                }
+                if (array[y] > array[i])
+                {
+                    int changer = array[i];
+                    array[i] = array[y];
+                    array[y] = changer;
+                    count = 0;
+                }
+                if (count == array.Length)
+                {
+                    break;
+                }
+            }
+            time.Stop();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array_line += Convert.ToString(array[i]) + " ";
+            }
+            textBox3.Text = array_line;
+            double convert = (double)time.ElapsedTicks;
+            textBox4.Text = Convert.ToString(convert);
+        }
+    }
+}
